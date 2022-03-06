e1ce4648176b5bfd84bd45a373c89ee2ad4f6cd3
nshan20 <58071671+nshan20@users.noreply.github.com>
2022-03-06 12:27:39 -0800

    das4

---
+++ Nshan_Antonyan_JS_das4/index.js
@@ -0,0 +1,52 @@
+//1
+// let value = prompt();
+// function reverseString(str) {
+//     var newString = "";
+//    for (let i = str.length-1; i >= 0; i--){
+//        newString += str[i];
+//    }
+//     return newString;
+// }
+// alert(reverseString(value));
+//-----------------------------------------------------------
+//2
+// let a = 5;
+// let b = 7;
+// let c = 6;
+//
+// function triangle(a,b,c){
+//     if ((a + b > c) && (c + b > a) && (c + a > b)){
+//         alert("hnaravor e");
+//     }else {
+//         alert("hnaravor che");
+//     }
+// }
+// triangle(a,b,c);
+//--------------------------------------------------------
+//3
+// let array  =  [1, 2, 2, 2, 3, 4, 2, 2, 3, 3, 4, 5];
+// let result = [];
+//
+// function sortingArr(arr){
+//     for (let i = 0; i < arr.length; i++){
+//         for (let j = i+1; j < arr.length; j++){
+//             if (arr[i] > arr[j]){
+//                 let sum = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = sum;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+// sortingArr(array);
+//
+// function filtrArr(eventSum){
+//     for (let index = 0; index < eventSum.length; index++){
+//         if (eventSum[index] !== eventSum[index +1]){
+//             result.push(eventSum[index]);
+//         }
+//     }
+// }
+// filtrArr(array);
+// console.log(result);
