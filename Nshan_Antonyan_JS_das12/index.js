07475713529fb8a09148b879fdfa3ee80b42d953
Your Name <58071671+nshan20@users.noreply.github.com>
2022-04-24 01:42:59 +0400

    update 0.2

--- Nshan_Antonyan_JS_das12/index.js
+++ Nshan_Antonyan_JS_das12/index.js
@@ -1,31 +1,62 @@
-let Titan_Button = document.querySelector("#Titan_Button");
-let Aluminum_Button = document.querySelector("#Aluminum_Button");
-let Plastic_button = document.querySelector("#Plastic_button");
+// let Titan_Button = document.querySelector("#Titan_Button");
+// let Aluminum_Button = document.querySelector("#Aluminum_Button");
+// let Plastic_button = document.querySelector("#Plastic_button");
 
-let Titan_input = document.querySelector("#Titan_input");
-let Aluminum_input = document.querySelector("#Aluminum_input");
-let Plastic_input = document.querySelector("#Plastic_input");
+// let Titan_input = document.querySelector("#Titan_input");
+// let Aluminum_input = document.querySelector("#Aluminum_input");
+// let Plastic_input = document.querySelector("#Plastic_input");
 
-let Material_spec = document.querySelector("#Material_spec");
-let Color_spec = document.querySelector("#Color_spec");
-let Count_spec = document.querySelector("#Count_spec");
-let button_spec = document.querySelector("#button_spec");
+// let Material_spec = document.querySelector("#Material_spec");
+// let Color_spec = document.querySelector("#Color_spec");
+// let Count_spec = document.querySelector("#Count_spec");
+// let button_spec = document.querySelector("#button_spec");
 
-let arr = [];
-let arr1 = [];
-let arr2 = [];
-let arr3 = [];
+// let arr = [];
+// let arr1 = [];
+// let arr2 = [];
+// let arrSpec = [];
 
 class Xanut {
-    constructor(pieces, arr, material) {
-        this.pieces = pieces;
-        this.arr = arr;
-        this.material = material;
+    constructor() {
+        // this.pieces = pieces;
+        // // this.arr = arr;
+        // this.material = material;
+
+        this.arrTitan = [];
+        this.arrAluminum = [];
+        this.arrPlastic = [];
+        this.arrSpec = [];
+
+        this.order(this.Titan_Button, this.Titan_input, this.arrTitan, "Titan");
+        this.order(this.Aluminum_Button, this.Aluminum_input, this.arrAluminum, "Aluminum");
+        this.order(this.Plastic_button, this.Plastic_input, this.arrPlastic, "Plastic");
+        this.order(this.button_spec, this.Count_spec, this.arrSpec, {"Material_spec":this.Material_spec, "Color_spec": this.Color_spec});
     }
-    itemSize() {
-        this.itemSize = this.pieces;
-        new Gorcaran(+this.itemSize, this.arr, this.material).multiplication();
-        return +this.itemSize;
+
+    Titan_Button = document.querySelector("#Titan_Button");
+    Aluminum_Button = document.querySelector("#Aluminum_Button");
+    Plastic_button = document.querySelector("#Plastic_button");
+
+    Titan_input = document.querySelector("#Titan_input");
+    Aluminum_input = document.querySelector("#Aluminum_input");
+    Plastic_input = document.querySelector("#Plastic_input");
+
+    Material_spec = document.querySelector("#Material_spec");
+    Color_spec = document.querySelector("#Color_spec");
+    Count_spec = document.querySelector("#Count_spec");
+    button_spec = document.querySelector("#button_spec");
+
+    order(Button, input, arr, material){
+        Button.addEventListener("click", ()=>{
+            this.itemSize(+input.value, arr, material);
+        });
+    }
+
+    itemSize( input, arr, material) {
+        // alert(input.value);
+        // this.itemSize = this.pieces;
+        new Gorcaran(input, arr, material).multiplication();
+
     }
 }
 
@@ -44,20 +75,20 @@ class Gorcaran {
         let material = this.material;
         let totalCost = 0;
         let mater = [
-            {title:"Titan", cen: 200, kg:3, color: "Black"},
-            {title:"Aluminum", cen: 100, kg:2, color: "Silver"},
-            {title:"Plastic", cen: 0, kg:1,color: "White" }
+            {title:"Titan", prices: 200, kg:3, color: "Black"},
+            {title:"Aluminum", prices: 100, kg:2, color: "Silver"},
+            {title:"Plastic", prices: 0, kg:1,color: "White" }
         ];
         for (let i = 0; i<mater.length; i++){
-            if (this.material === mater[i].title){
-                sum = new Aprank().cena + mater[i].cen;
+            if (material === mater[i].title){
+                sum = new Aprank().prices + mater[i].prices;
                 kg = new Aprank().kg + mater[i].kg;
                 color = mater[i].color;
                 test = false;
             }
         }
         if (test) {
-            sum = new Aprank().cena + 900;
+            sum = new Aprank().prices + 900;
             kg = new Aprank().kg + 5;
             color = this.material.Color_spec.value;
             material = this.material.Material_spec.value;
@@ -66,7 +97,7 @@ class Gorcaran {
             totalCost += sum;
             this.arr.push({id:i , "kg": kg," material": material ,"color": color, productPrice: sum});
         }
-        this.arr.push(totalCost + "$");
+        this.arr.push({totalAmount:totalCost + "$"});
         console.log(this.arr);
     }
 }
@@ -75,16 +106,35 @@ class Aprank {
     constructor() {
     }
     kg = 1;
-    cena = 100;
+    prices = 100;
 }
+new Xanut().order();
+// function order( Button, input, arr, material) {
+//     Button.addEventListener("click", function () {
+//         new Xanut(input.value, arr, material).itemSize();
+//     });
+// }
+//
+// order(Titan_Button, Titan_input, arr, "Titan");
+// order(Aluminum_Button, Aluminum_input, arr1, "Aluminum");
+// order(Plastic_button, Plastic_input, arr2, "Plastic");
+// order(button_spec, Count_spec, arr3, {"Material_spec":Material_spec, "Color_spec": Color_spec});
 
-function order( Button, input, arr, material) {
-    Button.addEventListener("click", function () {
-        new Xanut(input.value, arr, material).itemSize();
-    });
-}
+//-----------------------------------------------------------------------------------------------------
 
-order(Titan_Button, Titan_input, arr, "Titan");
-order(Aluminum_Button, Aluminum_input, arr1, "Aluminum");
-order(Plastic_button, Plastic_input, arr2, "Plastic");
-order(button_spec, Count_spec, arr3, {"Material_spec":Material_spec, "Color_spec": Color_spec});
+// class scrollHelper{
+//     constructor() {
+//         this.scrollElement = document.querySelector(".foo");
+//         this.scrollElement.addEventListener("click", () => {
+//             this.logSomething();
+//         });
+//     }
+//
+//    count = 1;
+//     logSomething(){
+//         console.log(this.count);
+//         this.count++;
+//     }
+// }
+//
+// new scrollHelper();
