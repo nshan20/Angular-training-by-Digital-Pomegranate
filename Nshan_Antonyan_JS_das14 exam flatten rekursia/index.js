50fedc1fc3f535a4dffa0cca40b4a21475c788c8
Your Name <58071671+nshan20@users.noreply.github.com>
2022-05-03 23:37:43 +0400

    calc

--- Nshan_Antonyan_JS_das14 exam flatten rekursia/index.js
+++ Nshan_Antonyan_JS_das14 exam flatten rekursia/index.js
@@ -25,65 +25,136 @@
 // -------------------------------------------------------------------------------------------------
 
 
-function example() {
-    fetch(`https://my-json-server.typicode.com/AshotAleqs/exams/expression?fbclid=IwAR08DeewkFyLOkdAOrghePq6UTcYPIZ9wKRy6IhnUPzEehbLGs6JD88Ly-c`)
+// function example() {
+//     fetch(`https://my-json-server.typicode.com/AshotAleqs/exams/expression?fbclid=IwAR08DeewkFyLOkdAOrghePq6UTcYPIZ9wKRy6IhnUPzEehbLGs6JD88Ly-c`)
+//         .then((data) => {
+//             return data.json();
+//         })
+//         .then((data) => {
+//             let serv1 = data.data.example1.input;
+//             console.log(tester(serv1));
+//         });
+// }
+//
+// example();
+//
+// function tester(dat) {
+//     let arry = dat.split("");
+//     let arrPrimer = ['(', ')', '{', '}', '[', ']', '<', '>'];
+//
+//     let obj = {};
+//
+//     for (let i = 0; i < arrPrimer.length; i++) {
+//         obj[arrPrimer[i]] = 0;
+//     }
+//
+//     for (let i = 0; i < arry.length; i++) {
+//         for (let j = 0; j < arrPrimer.length; j++) {
+//             if (arrPrimer[j] === arry[i]) {
+//                 if (obj[arry[i]]) {
+//                     obj[arry[i]] = obj[arry[i]] + 1;
+//                 } else {
+//                     obj[arry[i]] = 1;
+//                 }
+//             }
+//         }
+//     }
+//
+//     let operatorArray = [];
+//
+//     for (let kay in obj) {
+//         operatorArray.push(obj[kay]);
+//     }
+//
+//     let mas1 = [];
+//     let mas2 = [];
+//
+//     for (let i = 0; i < operatorArray.length; i++) {
+//         if (i % 2 === 0) {
+//             mas1.push(operatorArray[i]);
+//         } else {
+//             mas2.push(operatorArray[i]);
+//         }
+//     }
+//
+//     let TruthOrLie = true;
+//
+//     for (let i = 0; i < mas1.length; i++) {
+//         if (mas1[i] !== mas2[i]) {
+//             TruthOrLie = false;
+//         }
+//     }
+//
+//     return TruthOrLie;
+// }
+
+//------------------------------------------------------------------------------------------------
+//tnayin 4
+
+ function example() {
+    fetch(`https://my-json-server.typicode.com/AshotAleqs/exams/random_calculation?fbclid=IwAR3yklxprmQomyEti5tv1Q98nrCvO0LL6hmWzdWYJhCKmHVuaOYxYtu8CxY`)
         .then((data) => {
             return data.json();
         })
         .then((data) => {
             let serv1 = data.data.example1.input;
-            console.log(tester(serv1));
+            let testArr = [1,-3,5,-6,10];
+            calculator(serv1);
         });
 }
-
 example();
 
-function tester(dat) {
-    let arry = dat.split("");
-    let arrPrimer = ['(', ')', '{', '}', '[', ']', '<', '>'];
+function calculator(arr) {
+    console.log(arr);
+    let calcArr = [
+        {"plus":function (arg1, arg2){
+                return arg1 + arg2;
+            },title:"+"},
+        {"plus":function (arg1, arg2){
+                return arg1 - arg2;
+            },title:"-"},
+        {"plus":function (arg1, arg2){
+                return arg1 / arg2;
+            },title:"/"},
+        {"plus":function (arg1, arg2){
+                return arg1 * arg2;
+            },title:"*"}
+    ];
+    let truFals = true;
+    let sum = arr[0];
 
-    let obj = {};
+    let elementArr = 0;
+    // console.log(calcArr[0].plus(1,2));
 
-    for (let i = 0; i < arrPrimer.length; i++) {
-        obj[arrPrimer[i]] = 0;
-    }
+    for (let i = 1; i < arr.length; i++) {
+        if (truFals === true) {
+            // sum += arr[i];
+            sum = calcArr[elementArr].plus(sum,arr[i]);
 
-    for (let i = 0; i < arry.length; i++) {
-        for (let j = 0; j < arrPrimer.length; j++) {
-            if (arrPrimer[j] === arry[i]) {
-                if (obj[arry[i]]) {
-                    obj[arry[i]] = obj[arry[i]] + 1;
-                } else {
-                    obj[arry[i]] = 1;
-                }
+            alert(calcArr[elementArr].title);
+            alert(sum );
+            if (elementArr === 3){
+                elementArr = 0;
+            }else {
+                elementArr++;
+            }
+            if (arr[i] < 0) {
+                truFals = false;
+            }
+        } else {
+            // sum += arr[i];
+            sum = calcArr[elementArr].plus(sum,arr[i]);
+
+            alert( calcArr[elementArr].title );
+            alert( sum );
+            if (elementArr < 1){
+                elementArr = 3;
+            }else {
+                elementArr--;
+            }
+            if (arr[i] < 0) {
+                truFals = true;
             }
         }
     }
-
-    let operatorArray = [];
-
-    for (let kay in obj) {
-        operatorArray.push(obj[kay]);
-    }
-
-    let mas1 = [];
-    let mas2 = [];
-
-    for (let i = 0; i < operatorArray.length; i++) {
-        if (i % 2 === 0) {
-            mas1.push(operatorArray[i]);
-        } else {
-            mas2.push(operatorArray[i]);
-        }
-    }
-
-    let TruthOrLie = true;
-
-    for (let i = 0; i < mas1.length; i++) {
-        if (mas1[i] !== mas2[i]) {
-            TruthOrLie = false;
-        }
-    }
-
-    return TruthOrLie;
 }
