a8e2563e687c53a32e177124cf3676603894565f
Your Name <58071671+nshan20@users.noreply.github.com>
2022-05-06 13:16:50 +0400

    exams/random

--- Nshan_Antonyan_JS_das14 exam flatten rekursia/index.js
+++ Nshan_Antonyan_JS_das14 exam flatten rekursia/index.js
@@ -91,70 +91,113 @@
 //------------------------------------------------------------------------------------------------
 //tnayin 4
 
- function example() {
-    fetch(`https://my-json-server.typicode.com/AshotAleqs/exams/random_calculation?fbclid=IwAR3yklxprmQomyEti5tv1Q98nrCvO0LL6hmWzdWYJhCKmHVuaOYxYtu8CxY`)
-        .then((data) => {
-            return data.json();
-        })
-        .then((data) => {
-            let serv1 = data.data.example1.input;
-            let testArr = [1,-3,5,-6,10];
-            calculator(serv1);
-        });
-}
-example();
+//  function example() {
+//     fetch(`https://my-json-server.typicode.com/AshotAleqs/exams/random_calculation?fbclid=IwAR3yklxprmQomyEti5tv1Q98nrCvO0LL6hmWzdWYJhCKmHVuaOYxYtu8CxY`)
+//         .then((data) => {
+//             return data.json();
+//         })
+//         .then((data) => {
+//             let serv1 = data.data.example1.input;
+//             let testArr = [1,-3,5,-6,10];
+//             calculator(serv1);
+//         });
+// }
+// example();
+//
+// function calculator(arr) {
+//     console.log(arr);
+//     let calcArr = [
+//         {"plus":function (arg1, arg2){
+//                 return arg1 + arg2;
+//             },title:"+"},
+//         {"plus":function (arg1, arg2){
+//                 return arg1 - arg2;
+//             },title:"-"},
+//         {"plus":function (arg1, arg2){
+//                 return arg1 / arg2;
+//             },title:"/"},
+//         {"plus":function (arg1, arg2){
+//                 return arg1 * arg2;
+//             },title:"*"}
+//     ];
+//     let truFals = true;
+//     let sum = arr[0];
+//
+//     let elementArr = 0;
+//     // console.log(calcArr[0].plus(1,2));
+//
+//     for (let i = 1; i < arr.length; i++) {
+//         if (truFals === true) {
+//             // sum += arr[i];
+//             sum = calcArr[elementArr].plus(sum,arr[i]);
+//
+//             alert(calcArr[elementArr].title);
+//             alert(sum );
+//             if (elementArr === 3){
+//                 elementArr = 0;
+//             }else {
+//                 elementArr++;
+//             }
+//             if (arr[i] < 0) {
+//                 truFals = false;
+//             }
+//         } else {
+//             // sum += arr[i];
+//             sum = calcArr[elementArr].plus(sum,arr[i]);
+//
+//             alert( calcArr[elementArr].title );
+//             alert( sum );
+//             if (elementArr < 1){
+//                 elementArr = 3;
+//             }else {
+//                 elementArr--;
+//             }
+//             if (arr[i] < 0) {
+//                 truFals = true;
+//             }
+//         }
+//     }
+// }
 
-function calculator(arr) {
-    console.log(arr);
-    let calcArr = [
-        {"plus":function (arg1, arg2){
-                return arg1 + arg2;
-            },title:"+"},
-        {"plus":function (arg1, arg2){
-                return arg1 - arg2;
-            },title:"-"},
-        {"plus":function (arg1, arg2){
-                return arg1 / arg2;
-            },title:"/"},
-        {"plus":function (arg1, arg2){
-                return arg1 * arg2;
-            },title:"*"}
-    ];
-    let truFals = true;
-    let sum = arr[0];
-
-    let elementArr = 0;
-    // console.log(calcArr[0].plus(1,2));
-
-    for (let i = 1; i < arr.length; i++) {
-        if (truFals === true) {
-            // sum += arr[i];
-            sum = calcArr[elementArr].plus(sum,arr[i]);
-
-            alert(calcArr[elementArr].title);
-            alert(sum );
-            if (elementArr === 3){
-                elementArr = 0;
-            }else {
-                elementArr++;
-            }
-            if (arr[i] < 0) {
-                truFals = false;
-            }
-        } else {
-            // sum += arr[i];
-            sum = calcArr[elementArr].plus(sum,arr[i]);
-
-            alert( calcArr[elementArr].title );
-            alert( sum );
-            if (elementArr < 1){
-                elementArr = 3;
-            }else {
-                elementArr--;
-            }
-            if (arr[i] < 0) {
-                truFals = true;
-            }
-        }
-    }
-}
+// -------------------------------------------------------------------------
+// tnayin 3
+//  function example() {
+//     fetch(`https://my-json-server.typicode.com/AshotAleqs/exams/chain?fbclid=IwAR1OitylROFPotg2YKkDVHWsXAoFm_2GLY_qK-gwpWI4f3O1qSPRtrpYYbo`)
+//         .then((data) => {
+//             return data.json();
+//         })
+//         .then((data) => {
+//             let serv1 = data.data.example1.input_code;
+//             f(serv1);
+//         });
+// }
+// example();
+//
+// function f(fank){
+//     let fankshn = fank;
+//     console.log(fankshn);
+// }
+//
+// function add(num) {
+//     return num + 1;
+// }
+//
+// function mult(num) {
+//     return num * 30;
+// }
+//
+// function chain(sum, arry){
+//     let additionAmount = 0;
+//     let multiplicationSum = 0;
+//     for (let i = 0; i < arry.length; i++){
+//         if (i === 0){
+//             additionAmount = arry[i](sum);
+//         }else if (i === 1){
+//             multiplicationSum = arry[i](additionAmount);
+//         }
+//     }
+//     console.log(multiplicationSum);
+// }
+// chain(2, [add, mult]);
+//
+// function drop(num) { return num / 2;}  function doSomethingAsync(num) { return Promise.resolve(num * 3); } chain(10,[drop, doSomethingAsync]);
